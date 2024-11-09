973be20e871aeda4441062eb0bd110c603e2857a
malikadebiyi <malikbiyi@gmail.com>
2024-11-08 22:49:10 -0500

    CustomAuthEntryPoint

---
+++ feedapp/src/main/java/com/obsidi/feedapp/filter/CustomAuthEntryPoint.java
@@ -0,0 +1,26 @@
+package com.obsidi.feedapp.filter;
+
+import java.io.IOException;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CustomAuthEntryPoint implements AuthenticationEntryPoint {
+
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver resolver;
+
+    @Override
+    public void commence(HttpServletRequest req, HttpServletResponse res, AuthenticationException ex)
+            throws IOException {
+        resolver.resolveException(req, res, null, ex);
+    }
+}
