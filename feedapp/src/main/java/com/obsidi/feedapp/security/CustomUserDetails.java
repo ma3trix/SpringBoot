29dadb8741b1916b296eae4c45ea0f734f3e6f9f
malikadebiyi <malikbiyi@gmail.com>
2024-11-08 22:37:17 -0500

    CustomUserDetails

--- feedapp/src/main/java/com/obsidi/feedapp/security/CustomUserDetails.java
+++ feedapp/src/main/java/com/obsidi/feedapp/security/CustomUserDetails.java
@@ -1,5 +1,59 @@
 package com.obsidi.feedapp.security;
 
-public class CustomUserDetails {
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import com.obsidi.feedapp.jpa.User;
 
+public class CustomUserDetails implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    private User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // Returning null as we don't have specific authorities defined
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        // Return the user's password
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        // Return the user's username
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // Assuming the account is never expired
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        // Assuming the account is not locked
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // Assuming the credentials are never expired
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        // Assuming the account is enabled
+        return true;
+    }
 }
