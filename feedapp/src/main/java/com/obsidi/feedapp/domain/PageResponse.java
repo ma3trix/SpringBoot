631d6376baf73c15ec6999c32b594236de018789
malikadebiyi <malikbiyi@gmail.com>
2024-11-13 10:43:00 -0500

    PageResponse Class

---
+++ feedapp/src/main/java/com/obsidi/feedapp/domain/PageResponse.java
@@ -0,0 +1,54 @@
+package com.obsidi.feedapp.domain;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+
+public class PageResponse<T> {
+
+    private int pageNum;
+    private int pageSize;
+    private int totalPages;
+    private List<T> content;
+
+    public PageResponse(Page<T> paged) {
+        if (Optional.ofNullable(paged).isPresent()) {
+            this.pageNum = paged.getNumber();
+            this.pageSize = paged.getSize();
+            this.totalPages = paged.getTotalPages();
+            this.content = paged.getContent();
+        }
+    }
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public List<T> getContent() {
+        return content;
+    }
+
+    public void setContent(List<T> content) {
+        this.content = content;
+    }
+}
